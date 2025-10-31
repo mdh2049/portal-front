4a88e81abae252eb188c2f4396b1835650c3d8e0
dahye min <68039966+mdh2049@users.noreply.github.com>
2025-10-31 13:19:25 +0900

    refactor: align members sidebar with file manager

---
+++ src/components/modules/members/sidebar/MembersSidebar.tsx
@@ -0,0 +1,30 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import Button from 'components/base/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import TreeView from 'components/modules/file-manager/sidebar/TreeView';
+import StorageDetails from 'components/modules/file-manager/sidebar/StorageDetails';
+
+interface MembersSidebarProps {
+  setOpenOffcanvas: Dispatch<SetStateAction<boolean>>;
+}
+
+const MembersSidebar = ({ setOpenOffcanvas }: MembersSidebarProps) => {
+  return (
+    <div className="file-manager-sidebar scrollbar">
+      <div className="d-flex flex-between-center">
+        <h5 className="mb-2">My Files</h5>
+        <Button
+          className="p-0 fs-8 d-lg-none"
+          onClick={() => setOpenOffcanvas(false)}
+        >
+          <FontAwesomeIcon icon={faXmark} transform={'up-3'} />
+        </Button>
+      </div>
+      <TreeView />
+      <StorageDetails />
+    </div>
+  );
+};
+
+export default MembersSidebar;
